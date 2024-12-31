ee97f09c82b6ee3610857cae55ff615bb959e97c
Mseunghwan <minccc42710@gmail.com>
2024-12-31 09:34:45 +0900

    실행 확인

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,17 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.4.1"
-	id("io.spring.dependency-management") version "1.1.7"
+	kotlin("jvm") version "1.9.22"  // 먼저 kotlin plugin 선언
+	id("org.springframework.boot") version "3.2.2"
+	id("io.spring.dependency-management") version "1.1.4"
+	kotlin("plugin.spring") version "1.9.22"
+	kotlin("plugin.jpa") version "1.9.22"
 }
 
-group = "com.example"
+group = "com.example"  // your group id
 version = "0.0.1-SNAPSHOT"
+
+repositories {
+	mavenCentral()
+}
 
 java {
 	toolchain {
@@ -14,23 +19,14 @@ java {
 	}
 }
 
-repositories {
-	mavenCentral()
-}
-
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("com.h2database:h2")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
 }
 
 tasks.withType<Test> {
